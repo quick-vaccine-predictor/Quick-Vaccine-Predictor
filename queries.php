5de6f494c9fe8d3bc6536213c71eecc33b9fe250
NataliaSegura <44897028+NataliaSegura@users.noreply.github.com>
2019-02-19 16:29:01 +0000

    in progress

--- queries.php
+++ queries.php
@@ -57,45 +57,123 @@
       <!-- Main component for a primary marketing message or call to action -->
       <div class="row">
         <div class="col-sm-4">
-          <p>Sequence form</p>
+          <b>EPITOPE SEARCH</b>
           <form method="GET" name="sequenceForm" action="search.php">
-            <div class="form-group">
-              <label>Sequence </label>
-              <input type="text" name="sequenceName" value="" size="11" minlength="9" maxlength="10"/> 
-            </div>
-            <div class="form-group">
-              <label>HLA</label>
               <div class="form-check">
-                <?php
-                  foreach (array_keys($hlaTypeArray) as $idHlaType) {?>
-                    <table>
-                    <td><input class="form-check-input" type="checkbox" checked name="idHlaType[<?php print $idHlaType ?>]"/> <?php print $hlaTypeArray[$idHlaType]. "\n"?></td>
-                  <?php }
-                ?>
-                    </table>
-              </div>
-            </div>  
-            <button type="submit" class="btn btn-primary"> Submit </button>
+                <div class="radio">
+                  <label>Epitope sequence </label>
+                  <input type="text" name="sequenceName" value="" size="11" minlength="9" maxlength="10"/> 
+                </div>
+                <div class="radio">
+                  <label>Epitope ID </label>
+                  <input type="text" name="idEpitope" value="24" size="11" />
+                </div>
+              </div>    
+              <div class="form-group">
+                <label>HLA selection:</label> <br>
+                <select name="hla" multiple size="8">
+                        <?php
+                        foreach (array_keys($hlaTypeArray) as $idHlaType) {?>
+                        <option selected name="idHlaType[<?php print $idHlaType ?>]"><?php print $hlaTypeArray[$idHlaType]. "\n"?></option>
+                        <?php }
+                        ?>
+                    </select>
+                <div class="form-group">
+                <p>
+                  <label>Threshold for strong binder (nMAff) </label>
+                  <input type="text" name="sbaff" value="0.5" size="5"/>
+                  <label>Threshold for strong binder (logAff)</label>
+                  <input type="text" name="sblog" value="50" size="5"/>
+                </p>        
+                <p>
+                  <label>Threshold for weak binder (nMAff)</label>
+                  <input type="text" name="wbaff" value="2" size="5"/> 
+                  <label>Threshold for weak binder (logAff)</label>
+                  <input type="text" name="wblog" value="500" size="5"/> 
+                </p>
+                </div>
+              </div>  
+              <button type="submit" class="btn btn-primary"> Submit </button>
+              <button type="reset" value="Clear data" class="btn btn-primary">Clear data</button>
+
           </form>
         </div>
+        
         <div class="col-sm-4">
-          <p>Epitope ID search</p>
+          <b>ID SEARCH</b>
           <form method="GET" name="idForm" action="epitope.php">
-          <div class="form-group">
-            <label>Sequence </label>
-            <input type="text" name="idEpitope" value="24" size="11" />
+            <div class="form-group">
+              <label>Epitope ID </label>
+              <input type="text" name="idEpitope" value="24" size="11" />
+            </div>
+            <button type="submit" class="btn btn-primary"> Submit </button>
+          </form>
+          <form method="GET" name="sequenceForm" action="search.php">
+          <label>HLA ID </label>
+            <select name="hla" multiple size="8">
+              <?php
+                foreach (array_keys($hlaTypeArray) as $idHlaType) {?>
+                <option name="idHlaType[<?php print $idHlaType ?>]"><?php print $hlaTypeArray[$idHlaType]. "\n"?></option>
+                <?php }
+                ?>
+            </select>
+            <button type="submit" class="btn btn-primary"> Submit </button>      
+          </form>
+          <form method="GET" name="idForm" action="search.php">
+            <div class="form-group">
+              <label>Organism ID </label>
+              <input type="text" name="idOrganism" value="" size="20" minlength="4" maxlength="15"/> 
+            </div>
+            <button type="submit" class="btn btn-primary"> Submit </button>
+          </form> 
+          <form method="GET" name="idForm" action="search.php">
+            <div class="form-group">
+              <label>Organism name </label>
+              <input type="text" name="nameOrganism" value="" rows="2" size="50" minlength="0" maxlength="100"/> 
+            </div>
+            <button type="submit" class="btn btn-primary"> Submit </button>
+          </form> 
+          <form method="GET" name="idForm" action="search.php">
+            <div class="form-group">
+              <label>Protein ID </label>
+              <input type="text" name="idProtein" value="" rows="2" size="15" minlength="4" maxlength="30"/> 
+            </div>
+            <button type="submit" class="btn btn-primary"> Submit </button>
+          </form> 
+          <form method="GET" name="idForm" action="search.php">
+            <div class="form-group">
+              <label>Protein name </label>
+              <input type="text" name="nameProtein" value="" rows="2" size="50" minlength="0" maxlength="100"/> 
+            </div>
+            <button type="submit" class="btn btn-primary"> Submit </button>
+          </form> 
+          <form method="GET" name="idForm" action="search.php">
+            <div class="form-group">
+              <label>Antigen ID </label>
+              <input type="text" name="idAntigen" value="" rows="2" size="15" minlength="4" maxlength="30"/> 
+            </div>
+            <button type="submit" class="btn btn-primary"> Submit </button>
+          </form> 
+          <form method="GET" name="idForm" action="search.php">
+            <div class="form-group">
+              <label>Antigen name </label>
+              <input type="text" name="nameAntigen" value="" rows="2" size="50" minlength="0" maxlength="100"/> 
+            </div>
+            <button type="submit" class="btn btn-primary"> Submit </button>
+          </form> 
           </div>
-          <button type="submit" class="btn btn-primary"> Submit </button>
-        </form>
-        </div>      
         <div class="col-sm-4">
-          <p>Proteosome</p>
+          <b>GENOME</b>
           <form method="POST" name="proteosomeForm" action="proteosome.php">
             <div class="form-group">
-              <label>Sequence </label>
+              <label>Whole viral Genome Sequence </label>
               <textarea class="form-control" rows="5" value="" type="text" name="proteosomeText"></textarea>
+              <input name="uploadFile" type="file"><br>
             </div>
             <div class="form-check">
+              <div class="radio">
+              <label><input type="radio" name="in_dna" value="2">RNA</label>
+              </div>
               <div class="radio">
               <label><input type="radio" checked name="in_dna" value="1">DNA</label>
               </div>
@@ -113,19 +191,11 @@
               <label><input type="radio" name="length" value="2">Both</label>
               </div>
               <button type="submit" class="btn btn-primary"> Submit </button>
+              <button type="reset" value="Clear data" class="btn btn-primary">Clear data</button>
             </div>
           </form>
         </div>
-        <p>HLA ID search</p>
-          <form method="GET" name="idForm" action="hla.php">
-          <div class="form-group">
-            <label>Sequence </label>
-            <input type="text" name="idHLA" value="HLA00001" size="11" />
-          </div>
-          <button type="submit" class="btn btn-primary"> Submit </button>
-        </form>
       </div>
-
     </div> <!-- /container -->
 
 
@@ -137,6 +207,16 @@
     <script src="css/bootstrap.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="css/ie10-viewport-bug-workaround.js"></script>
+    <!-- to write or epitope sequence or ID, not both -->
+    <script type="text/javascript">
+      $("input[name=sequenceName]").focus(function() {
+        $("input[name=idEpitope]").val('');
+      });
+
+      $("input[name=idEpitope]").focus(function() {
+        $("input[name=sequenceName]").val('');
+      });
+    </script>
   
 
 </body></html>
