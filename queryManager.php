7caeaad80f583ddf4253fac61ae26d22087f32b3
PauBadiaM <44896790+PauBadiaM@users.noreply.github.com>
2019-02-25 14:52:55 +0100

    Add files via upload

--- queryManager.php
+++ queryManager.php
@@ -15,6 +15,9 @@ elseif (isset($_GET["nameAntigen"])) {
 	$name = $_GET["nameAntigen"];
 	$sql = "SELECT nameAntigen, idAntigen FROM Antigen WHERE nameAntigen LIKE '%".$name."%';";
 }
+elseif (isset($_POST["sequenceName"])) {
+	$sql = "";
+}
 $nameTable = $conn->query($sql);
 $_SESSION["array"] = mysqli_fetch_all($nameTable);
 $conn->close();
@@ -31,8 +34,20 @@ print navbar('Epitope');
         <table class="table table-striped table-sm table-responsive" id="nameTable">
           <thead>
             <tr>
-              <th>Organism Name</th>
-              <th>Organism Id</th>
+            <?php 
+            if (isset($_GET["nameOrganism"])){ 
+            	echo "<th>Organism Name</th><br><th>Organism Id</th>";
+            }
+            elseif (isset($_GET["nameProtein"])){
+            	echo "<th>Protein Name</th><br><th>Protein Id</th>";
+            }
+            elseif (isset($_GET["nameAntigen"])){
+            	echo "<th>Antigen Name</th><br><th>Antigen Id</th>";
+            }
+            elseif (isset($_POST["sequenceName"])){
+            	echo "<th>Epitope Id</th><br><th>Sequence</th><br><th>HLA</th><br><th>log</th><br><th>nM</th><br>";
+            }
+            ?>
             </tr>
           </thead>
       		<tbody>
@@ -43,14 +58,14 @@ print navbar('Epitope');
 		      			</th>
 		      			<td class='text-center'><?php $id = $row["idOrganism"];
 		      				echo "<a href='organism.php?idOrganism=$id' target='_blank'>{$id}</a>";
-		      			} ?>
-		      			<?php if (isset($_GET["nameProtein"])){ ?>
+		      			} 
+		      			elseif (isset($_GET["nameProtein"])){ ?>
 		      			<th scope='row' class='text-center'><?php echo $row["nameProtein"]?>	
 		      			</th>
 		      			<td class='text-center'><?php $id = $row["idProtein"];
 		      				echo "<a href='protein.php?idProtein=$id' target='_blank'>{$id}</a>";
-		      			} ?>
-		      			<?php if (isset($_GET["nameAntigen"])){ ?>
+		      			}
+		      			elseif (isset($_GET["nameAntigen"])){ ?>
 		      			<th scope='row' class='text-center'><?php echo $row["nameAntigen"]?>	
 		      			</th>
 		      			<td class='text-center'><?php $id = $row["idAntigen"];
