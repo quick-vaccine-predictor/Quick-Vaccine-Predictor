0e3e873024e652fa335ea6d87ebf9a4348d06c33
PauBadiaM <44896790+PauBadiaM@users.noreply.github.com>
2019-02-25 15:00:09 +0100

    Update queries.php

--- queries.php
+++ queries.php
@@ -1,7 +1,6 @@
 <?php 
 
 // Start the session
-session_start();
 
 // Check if the user is logged in, if not then redirect him/her to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
