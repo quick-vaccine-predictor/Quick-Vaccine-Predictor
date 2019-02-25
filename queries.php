a656cb221b2d8b7fd30ad94ab6c8c4993006285d
PauBadiaM <44896790+PauBadiaM@users.noreply.github.com>
2019-02-25 15:09:26 +0100

    Update queries.php

--- queries.php
+++ queries.php
@@ -3,10 +3,10 @@
 // Start the session
 
 // Check if the user is logged in, if not then redirect him/her to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-  header("location: login.php");
-  exit;
-}
+//if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+//  header("location: login.php");
+//  exit;
+//}
 
 include "hlaTypeArray.php";
 include("globals.inc.php");
