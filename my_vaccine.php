66b23187ed4a82ba880779b58199a59889a4c384
NataliaSegura <44897028+NataliaSegura@users.noreply.github.com>
2019-02-25 16:57:10 +0000

    Add files via upload
    last 'changes'

--- my_vaccine.php
+++ my_vaccine.php
@@ -3,22 +3,20 @@ include("globals.inc.php");
 $title = "my Vaccine";
 print headerDBW($title);
 
-//Conection to the DB if needed
-//$conn = connectSQL();
-//$conn->close();
-//session_start();
-
 // Check if the user is logged in, if not then redirect him to login page
 //if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-//    header("location: login.php");
+//    header("location: ./usr_register/login.php");
 //    exit;
 //}
 
-$sequence = $_GET["sequenceName"];
+//Conection to the DB
 $conn = connectSQL();
-$sql = "SELECT idUser, mailUser FROM User";
+$sql = "SELECT User.idUser, mailUser, Vaccine.idVaccine, nameVaccine, idEpitope from User JOIN Vaccine ON User.idUser = Vaccine.idUser JOIN VaccineContent ON Vaccine.idVaccine = VaccineContent.idVaccine WHERE isUser = $idUser";
 $result = $conn->query($sql); /* the search is done here */  
 $conn->close();
+
+
+
 
 
 print navbar('myVaccine');
@@ -32,12 +30,13 @@ print navbar('myVaccine');
         </div>
         <div class="col-sm-4">
             tutorial
+            <br>
+            <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+            <br>
+            <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
         </div>
     </div>
-    <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
-        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
-    </p>
+
     <div class="row">
         <h2>Epitopes</h2>
         <table class="table table-striped table-sm table-responsive" id="affTable">
@@ -45,13 +44,13 @@ print navbar('myVaccine');
             <tr>
               <th>Vaccine name</th>
               <th>idEpitope</th>
-+            </tr>
+            </tr>
           </thead>
       <tbody>
         <?php while($row = $result->fetch_assoc()) { ?>
           <tr>
-            <th scope='row' id="idUser"><a href="<?php echo $row['idUser'] ?>"><?php echo $row['idUser'] ?></th>
-            <td class='text-center' id="mail"><?php echo $row['mailUser'] ?></td>
+            <th scope='row' id="nameVaccine"><a href="<?php echo $row['nameVaccine'] ?>"></th>
+            <td class='text-center' id="idEpitope"><?php echo $row['idEpitope'] ?><?php echo $row['idUser'] ?></td>
           </tr>
         <?php } ?>
       </tbody>
