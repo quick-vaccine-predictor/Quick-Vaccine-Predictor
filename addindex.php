2fcf794746140d0e62dd085d79c486b3610809e3
NataliaSegura <44897028+NataliaSegura@users.noreply.github.com>
2019-02-27 13:52:54 +0000

    Add files via upload
    ara ja ens poden pujar a la DB

--- addindex.php
+++ addindex.php
@@ -8,40 +8,37 @@ $title = "add";
 print headerDBW($title);
 
 // Check if the user is logged in, if not then redirect him to login page
-//if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-//    header("location: login.php");
-//    $email= $_SESSION["email"];
-//    exit;
-//  }
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    $email= $_SESSION["email"];
+    exit;
+  }
 
 //Conection to the DB if needed
 $conn = connectSQL();
 
 // Iniziatializing variables
 $nVaccine = "";
+$idUser="";
 $errors = array();
 
 // Register User
-if(isset($_POST) & !empty($_POST)){
+if(isset($_POST["addbutton"]) & !empty($_POST)){
 	//Recieve all input values from the form
     $nVaccine = $_POST['nVaccine'];
-
+    $idUser = $_SESSION['idUser'];
 	//Form validation by adding corresponding errors into $errors array
-    if(empty($nVaccine)) { echo "er"; array_push($errors, "nameVaccine is required"); }
-
-    echo "<br>" ;
-    echo "a";
-    echo "<br>" ;
+    if(empty($nVaccine)) { array_push($errors, "nameVaccine is required"); }
 }
 
 // If there is no errors in the form register user:
 	if (count($errors) == 0) {
         //$sql = "INSERT INTO Vaccine set nameVaccine = $nVaccine, idUser = (SELEC idUser FROM User WHERE idUser='6')";
-        $sql = "INSERT INTO Vaccine SET nameVaccine = '$nVaccine' , idUser = (SELECT idUser FROM User WHERE idUser = '7')";
-        $sql2 = "INSERT INTO VaccineConent SET idVaccine = (SELECT idVaccine FROM Vaccine), idEpitope = '41'";
+        $sql = "INSERT INTO Vaccine SET nameVaccine = '$nVaccine' , idUser ='$idUser'";
+        //$sql2 = "INSERT INTO VaccineConent SET idVaccine = (SELECT idVaccine FROM Vaccine), idEpitope = '41'";
 
         mysqli_query($conn, $sql);
-        mysqli_query($conn, $sql2);
+        //mysqli_query($conn, $sql2);
         echo "1 record added";
 	}
 
@@ -57,8 +54,8 @@ print navbar('HLA');
     <form action="" method="post">
         nameVaccine: <input type="text" name="nVaccine" /><br><br>
         <!--<input type="submit" />-->
-        <button id='<?php echo $_SESSION["idEpitope"].$_SESSION["nameHLA"]?>' type='reset' name='addbutton'>add</button>
-
+        <input id='<?php echo $_SESSION["idEpitope"]?>' type='submit' name='addbutton' value="myVaccine">
+        <!--echo $_SESSION["idEpitope"].$_SESSION["nameHLA"]?>-->
     </form>
 </div>
 
