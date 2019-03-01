50f914ca59d1d15ca1a4711e2e224cdb77049c1a
PauBadiaM <44896790+PauBadiaM@users.noreply.github.com>
2019-03-01 15:02:24 +0100

    Add files via upload

--- queries.php
+++ queries.php
@@ -1,14 +1,4 @@
 <?php 
-
-// Start the session
-
-// Check if the user is logged in, if not then redirect him/her to login page
-//if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-//  header("location: login.php");
-//  exit;
-//}
-
-include "hlaTypeArray.php";
 include("globals.inc.php");
 print headerDBW("Queries");
 print navbar('Queries');
