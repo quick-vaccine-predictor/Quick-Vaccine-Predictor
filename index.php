e73b7563e8173ebd6e00866a062995be406b11e6
PauBadiaM <44896790+PauBadiaM@users.noreply.github.com>
2019-03-04 20:07:28 +0100

    Responsive image added

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ print navbar('Home');
       <!-- Main component for a primary marketing message or call to action -->
       <div class="jumbotron text-justify" style="background:transparent !important">
         <div>
-          <h1>Welcome to Quick Vaccine Viral Predictor</h1>
+          <h1>Welcome to Quick Vaccine Predictor</h1>
         </div>  
           <p>QVVP is a public application designed to help speed up the generation of new viral vaccines. 
               It is a student's project from the Mc in Bioinformatics in Health Science from Universitat Pompeu Fabra (UPF).
-              QVVP contains a database with information of more than 41.000 viral epitopes from IEDB. Furthermore, it contains
+              QVP contains a database with information of more than 41.000 viral epitopes from IEDB. Furthermore, it contains
               already calculated binding simulations for each epitope using netMHCcons, a reliable binding prediction software from DTU Bioinformatics (Denmark).
           </p>
           <p></p>
@@ -24,7 +24,7 @@ print navbar('Home');
         <h2 class="featurette-heading">The concept. <span class="text-muted">How can we speed up vaccine production?</span></h2>
         <p class="lead">The problem with vaccine design is that, for each new viral protein there can be L - k + 1 potentialy useful epitopes (where L is the length of the protein and k the length of the epitope). Thus, in a simple protein of 500 aa and with epitopes of 9 aa, 492 epitopes should be tested in assays to determine if they could be used in a vaccine.</p>
         <div class ='text-center'>
-        <img class="img-rounded" src="png/qvvp_idea.png" width="500" height="300"><br><br>
+        <img class="img-rounded img-responsive center-block" src="png/qvvp_idea.png" width="500"><br><br>
         </div>
         <p class="lead">To speed this expensive and time consuming process, we selected all viral epitopes of 9 and 10 aa from IEDB which we then performed time-consuming computer simulations of epitope binding and finally stored the results in our database.<br>
         With this novel data, researchers can perform several queries to determine which epitopes show more potential to perform in a vaccine.</p>
