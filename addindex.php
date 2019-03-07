897a17bd7c30adcf365f3c86945f452715f8bab1
PauBadiaM <44896790+PauBadiaM@users.noreply.github.com>
2019-03-07 09:43:52 +0100

    Added feature when empty vaccines

--- addindex.php
+++ addindex.php
@@ -7,6 +7,12 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
   }
 print navbar('MyVaccine');
+$conn = connectSQL();
+$idUser = $_SESSION["idUser"];
+$sql = "SELECT idVaccine, nameVaccine from Vaccine WHERE idUser = '$idUser'";
+$vaccineTable = $conn->query($sql);
+$vaccine_arr = array();
+$conn->close();
 ?>
 <div class="container">
     <?php echo $_SESSION["status"]; $_SESSION["status"] = ""; ?>
@@ -17,19 +23,15 @@ print navbar('MyVaccine');
         <button type='submit' class="btn btn-info btn-sm">Add</button>
         <button onclick="self.close()" class="btn btn-warning btn-sm">Cancel</button>
     </form>
-    <form action="addlinker.php" method="GET">
+    <form action="addlinker.php" method="GET" id="existingVacc">
         <div class="form-group">
             <label>Insert into an existing vaccine:</label> <br>
             <select name="idVaccine">
                 <?php
-                $conn = connectSQL();
-                $idUser = $_SESSION["idUser"];
-                $sql = "SELECT idVaccine, nameVaccine from Vaccine WHERE idUser = '$idUser'";
-                $vaccineTable = $conn->query($sql);
-                $conn->close();
                 foreach ($vaccineTable as $vaccinerow) {
                     $nameVaccine = $vaccinerow["nameVaccine"];
                     $idVaccine = $vaccinerow["idVaccine"];
+                    $vaccine_arr[] = $idVaccine;
                   ?>
                 <option selected value="<?php echo $idVaccine ?>"><?php echo $nameVaccine?></option>
                 <?php                
@@ -42,5 +44,14 @@ print navbar('MyVaccine');
         </div>
     </form>
 </div>
+<script type="text/javascript">
+    var vaccine_json = '<?php echo json_encode($vaccine_arr) ?>';
+    if (vaccine_json == '[]') {
+        $('#existingVacc').hide();
+    } else {
+        $('#existingVacc').show();
+    }
 
+
+</script>
 <?php print footerDBW();?>
